2031e230e42a65b9ba22750d1db0a96805b67280
Ben Thomas <44470695+Xiphias5985@users.noreply.github.com>
2022-06-08 11:11:38 +1000

    Update visionService.js
    Wrapped async functions in try/catch blocks.

--- services/visionService.js
+++ services/visionService.js
@@ -1,105 +1,48 @@
-require('dotenv').config();
-
-const vision = require('@google-cloud/vision');
-
-// Load Sugar model
 const Sugar = require('../models/Sugars');
 
-const SUGARS_DB = ['sugar','dextrose','fructose','galactose','glucose','lactose','maltose',
-                'sucrose','demerara','syrup','rapadura','dextrin','diastatic malt','maltol',
-                'muscovado','panela','maltodextrin','turbinado','sucanat','molasses','agave',
-                'high fructose','hfcs','honey','treacle','buttercream','caramel', 'juice concentrate'];
-
-function filterSugars(arr, query) {
-    return arr.filter(function(el) {
-        return el.toLowerCase().indexOf(query.toLowerCase()) !== -1
-    })
-};
-
-function union(setA, setB) {
-    let _union = new Set(setA)
-    for (let elem of setB) {
-        if (elem.includes("ingredients") == false) {
-            _union.add(elem)
-        }
-    }
-    return _union
-};
-
-const extractLabel = async (req, res) => {
+const getAllSugars = async (req, res) => {
     try {
-        let img_path = req
-        if(img_path) {
-            // Create a client
-            const client = new vision.ImageAnnotatorClient(credentials=process.env.GOOGLE_APPLICATION_CREDENTIALS);
-
-            console.log("GCP client connected")
-            // Performs text detection on the local file
-            const [result] = await client.textDetection(img_path);
-            const detections = result.textAnnotations;
-            if (detections.length != 0){
-                let label = detections.shift().description;
-                if(label) {
-                    return label
-                }
-            }
-        }
-        return null
+        let userID = req;
+        let sugars = await Sugar.find({userID: userID}).sort({date: -1}).exec();
+        return sugars
     } catch (error) {
         console.log(error);
     }
 };
 
-const extractSugars = (req, res) => {
-    if(req.label) {
-        var sugarsData = {
-            productName: req.productName,
-            userID: req.userID
-        }
-        console.log("Extracting sugars")
-        let label = req.label.replace(/\n/g, " "); // replace new line char with space
-        let ingredients = []
-        let words = label.split(/[,,.,:,(,),\[,\]]/);
-        words.forEach((s) => {
-            s = s.replace(/[^a-zA-Z\s]/g, ""); //remove non alpha characters
-            s = s.trim() //remove trailing/starting whitespace
-            s = s.toLowerCase() //convert to lowercase
-            ingredients.push(s)
-        });
-        // split label string to list of words and trim whitespace
-        var allSugars = new Set();
-        SUGARS_DB.forEach((sugar) => {
-            let filtered = filterSugars(ingredients, sugar);
-            let matched = new Set(filtered);
-            allSugars = union(allSugars, matched);
-        });
-        let sugarsList = Array.from(allSugars);
-        if (sugarsList.length != 0) {
-            sugarsData['sugars'] = sugarsList;
-        }
-        else {
-            sugarsData['sugars'] = ["No sugars detected"];
-        }
-
-        saveSugars(sugarsData)
-
+const getLatestSugar = async (req, res) => {
+    try {
+        let userID = req;
+        let sugars = await Sugar.findOne({userID: userID}).sort({date: -1}).exec();
+        return sugars
+    } catch (error) {
+        console.log(error);
     }
-    else {
-        res.json({
-            statusCode: 400,
-            message: "Failed: Cannot extract sugars from label"
-        });
-    };
 };
 
-const saveSugars = (sugarsData) => {
-    Sugar.create(sugarsData, (err, result) => {
-        if(result) {
-            console.log("Sugars saved", result._id);
-        }
-    });
-}
+const updateProductName = async (req, res) => {
+    try {
+        let objID = req.objID;
+        let newName = req.newName;
+        let updated = await Sugar.updateOne({_id: objID},{productName:newName}).exec();
+        console.log("Product name updated");
+        return updated
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const deleteByID = async (req, res) => {
+    try {
+        let objID = req;
+        let deleted = await Sugar.deleteOne({_id: objID}).exec();
+        console.log("Product deleted");
+        return deleted
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 module.exports = {
-    extractLabel, extractSugars
+    getAllSugars, getLatestSugar, updateProductName, deleteByID
 }
