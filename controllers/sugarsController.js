a21d4c3adcbb4e9844aaa78a003f83b37ff42123
Ben Thomas <44470695+Xiphias5985@users.noreply.github.com>
2022-06-08 11:10:16 +1000

    Update sugarsController.js
    Wrapped async function in try/catch block.

--- controllers/sugarsController.js
+++ controllers/sugarsController.js
@@ -1,107 +1,119 @@
 let Service = require("../services");
 
 const getUserSugars = async (req, res) => {
-    if (req) {
-        let result = await Service.sugarsService.getAllSugars(req);
-        if(result) {
-            res.json({
-                statusCode:200,
-                data: result,
-                message: "Sucess: retrieved all results for user"
-            });
+    try {
+        if (req) {
+            let result = await Service.sugarsService.getAllSugars(req);
+            if(result) {
+                res.json({
+                    statusCode:200,
+                    data: result,
+                    message: "Sucess: retrieved all results for user"
+                });
+            }
+            else {
+                res.json({
+                    statusCode:400,
+                    message: "Failed: could not retrieve results for user"
+                });
+            }
         }
         else {
             res.json({
-                statusCode:400,
-                message: "Failed: could not retrieve results for user"
+                statusCode: 400,
+                message: "Failed: No userID"
             });
         }
+    } catch (error) {
+        console.log(error)
     }
-    else {
-        res.json({
-            statusCode: 400,
-            message: "Failed: No userID"
-        });
-    }
-
 };
 
 const updateProduct= async (req, res) => {
-    if (req) {
-        let result = await Service.sugarsService.updateProductName(req);
-        if(result) {
-            res.json({
-                statusCode:200,
-                data: result,
-                message: "Sucess: product name updated"
-            });
+    try {
+        if (req) {
+            let result = await Service.sugarsService.updateProductName(req);
+            if(result) {
+                res.json({
+                    statusCode:200,
+                    data: result,
+                    message: "Sucess: product name updated"
+                });
+            }
+            else {
+                res.json({
+                    statusCode:400,
+                    message: "Failed: could not update product name"
+                });
+            }
         }
         else {
             res.json({
-                statusCode:400,
-                message: "Failed: could not update product name"
+                statusCode: 400,
+                message: "Failed: No product ID or name provided"
             });
         }
+    } catch (error) {
+        console.log(error);
     }
-    else {
-        res.json({
-            statusCode: 400,
-            message: "Failed: No product ID or name provided"
-        });
-    }
-
 };
 
 const deleteProduct = async (req, res) => {
-    if (req) {
-        let result = await Service.sugarsService.deleteByID(req);
-        if(result) {
-            res.json({
-                statusCode:200,
-                data: result,
-                message: "Sucess: product deleted"
-            });
+    try {
+        if (req) {
+            let result = await Service.sugarsService.deleteByID(req);
+            if(result) {
+                res.json({
+                    statusCode:200,
+                    data: result,
+                    message: "Sucess: product deleted"
+                });
+            }
+            else {
+                res.json({
+                    statusCode:400,
+                    message: "Failed: could not delete product"
+                });
+            }
         }
         else {
             res.json({
-                statusCode:400,
-                message: "Failed: could not delete product"
+                statusCode: 400,
+                message: "Failed: No product ID provided"
             });
         }
+    } catch (error) {
+        console.log(error);
     }
-    else {
-        res.json({
-            statusCode: 400,
-            message: "Failed: No product ID provided"
-        });
-    }
-
 };
 
 const getUserResult = async (req, res) => {
-    if (req) {
-        let result = await Service.sugarsService.getLatestSugar(req);
-        if(result) {
-            res.json({
-                statusCode:200,
-                data: result,
-                message: "Sucess: retrieved latest sugar for user"
-            });
+    try {
+        if (req) {
+            let result = await Service.sugarsService.getLatestSugar(req);
+            if(result) {
+                res.json({
+                    statusCode:200,
+                    data: result,
+                    message: "Sucess: retrieved latest sugar for user"
+                });
+            }
+            else {
+                res.json({
+                    statusCode:400,
+                    message: "Failed: could not retrieve result for user"
+                });
+            }
         }
         else {
             res.json({
-                statusCode:400,
-                message: "Failed: could not retrieve result for user"
+                statusCode: 400,
+                message: "Failed: No userID"
             });
         }
+    } catch (error) {
+        console.log(error);
     }
-    else {
-        res.json({
-            statusCode: 400,
-            message: "Failed: No userID"
-        });
-    }
-
 };
 
 module.exports = {
